cbfff2a01cf2d0377b2d613457476fb15f24bc37
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2023-12-18 21:49:20 -0300

    Alteracao Ques02 DadosPessoais (Mais velho +novo)

--- Struct/atividade_structs_quest2.c
+++ Struct/atividade_structs_quest2.c
@@ -1,97 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dados_pessoal{
+// Estrutura para armazenar dados pessoais
+struct DadosPessoais {
     char nome[20];
     int idade;
     int cpf;
 };
 
-void preenche(struct dados_pessoal *pessoas){
+// Função para preencher os dados pessoais
+void preencherDados(struct DadosPessoais *pessoa) {
     printf("Digite o nome: ");
-    scanf(" %[^\n]", pessoas -> nome);
+    scanf(" %[^\n]", pessoa -> nome);
 
     printf("Digite a idade: ");
-    scanf("%d", &pessoas -> idade);
+    scanf("%d", &pessoa -> idade);
 
-    printf("Digite o cpf: ");
-    scanf("%d", &pessoas -> cpf);
+    printf("Digite o CPF: ");
+    scanf("%d", &pessoa -> cpf);
 } 
 
-void imprimir(struct dados_pessoal *pessoas){
-    printf("Nome: %s\n", pessoas -> nome);
-    printf("Idade: %d\n", pessoas -> idade);
-    printf("CPF: %d\n", pessoas -> cpf);
+// Função para imprimir os dados pessoais
+void imprimirDados(struct DadosPessoais *pessoa) {
+    printf("Nome: %s\n", pessoa -> nome);
+    printf("Idade: %d\n", pessoa -> idade);
+    printf("CPF: %d\n", pessoa -> cpf);
 }
 
-void alterar_idade(struct dados_pessoal *pessoas){
-    printf("Digite a nova idade:");
-    scanf("%d", &pessoas -> idade);
+// Função para alterar a idade
+void alterarIdade(struct DadosPessoais *pessoa) {
+    printf("Digite a nova idade: ");
+    scanf("%d", &pessoa -> idade);
 }
 
+// Função para encontrar a pessoa mais velha e mais nova
+void encontrarMaisVelhoEMaisNovo(struct DadosPessoais *pessoas, int quantidade) {
+    int idadeMaisVelha = pessoas[0].idade;
+    int idadeMaisNova = pessoas[0].idade;
+    int indiceMaisVelho = 0;
+    int indiceMaisNovo = 0;
 
-void maior_e_menor(struct dados_pessoal *pessoas, int n){
-    int maior_idade = pessoas[0].idade;
-    int menor_idade = pessoas[0].idade;
-    int posicao_maior = 0;
-    int posicao_menor = 0;
-
-    for (int i = 0; i < n; i++){
-        if(pessoas[i].idade > maior_idade){
-            maior_idade = pessoas[i].idade;
-            posicao_maior = i;
+    for (int i = 0; i < quantidade; i++) {
+        if(pessoas[i].idade > idadeMaisVelha) {
+            idadeMaisVelha = pessoas[i].idade;
+            indiceMaisVelho = i;
         }
-        else if(pessoas[i].idade < menor_idade){
-            menor_idade = pessoas[i].idade;
-            posicao_menor = i;
+        else if(pessoas[i].idade < idadeMaisNova) {
+            idadeMaisNova = pessoas[i].idade;
+            indiceMaisNovo = i;
         }
     }
 
     printf("A pessoa mais velha é: %s\n"
-    "com a idade: %d", pessoas[posicao_maior].nome, pessoas[posicao_maior].idade);
+    "com a idade: %d\n", pessoas[indiceMaisVelho].nome, pessoas[indiceMaisVelho].idade);
 
     printf("A pessoa mais nova é: %s\n"
-    "com a idade: %d", pessoas[posicao_menor].nome, pessoas[posicao_menor].idade);
-
-
+    "com a idade: %d\n", pessoas[indiceMaisNovo].nome, pessoas[indiceMaisNovo].idade);
 }
 
-int main(void){
+int main(void) {
+    int numPessoas, posicao, opcao;
+    printf("Digite o número de pessoas: ");
+    scanf("%d", &numPessoas);
 
-    int n, posicao, op;
-    printf("Digite o numero de pessoas: ");
-    scanf("%d", &n);
+    struct DadosPessoais *pessoas = (struct DadosPessoais*) malloc (numPessoas * sizeof(struct DadosPessoais));
 
-    struct dados_pessoal *pessoas = (struct dados_pessoal*) malloc (n * sizeof(struct dados_pessoal));
-
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < numPessoas; i++) {
         printf("------------------------------------\n");
         printf("Digite os dados da pessoa %d\n", i+1);
-        preenche(&pessoas[i]);
+        preencherDados(&pessoas[i]);
     }
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < numPessoas; i++) {
         printf("------------------------------------\n");
         printf("Dados da pessoa %d\n", i+1);
-        imprimir(&pessoas[i]);
+        imprimirDados(&pessoas[i]);
     }  
 
-
     printf("Deseja alterar a idade de alguma pessoa? (1 - sim, 2 - não): ");
-    scanf("%d", &op);
+    scanf("%d", &opcao);
 
-
-    if(op ==1){
-
-    
-    printf("Digite a posição da pessoa que deseja alterar a idade: ");
-    scanf("%d", &posicao);
-
-    alterar_idade(&pessoas[posicao]);
+    if(opcao == 1) {
+        printf("Digite a posição da pessoa que deseja alterar a idade: ");
+        scanf("%d", &posicao);
+        alterarIdade(&pessoas[posicao]);
     }
 
-    maior_e_menor(pessoas, n);
-
+    encontrarMaisVelhoEMaisNovo(pessoas, numPessoas);
 
     free(pessoas);
     return 0;
