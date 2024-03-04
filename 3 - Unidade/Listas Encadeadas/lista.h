13d626e54b301d4d4443a257474b2ef03a62230d
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2024-03-04 16:10:39 -0300

    Encapsulando as funcoes Lista Enc

---
+++ 3 - Unidade/Listas Encadeadas/lista.h
@@ -0,0 +1,33 @@
+
+typedef struct lista Lista;
+/*
+Funcao que inicializa a lista
+atribuindo NULL
+Retorna lista inicializada
+*/
+
+Lista * lst_cria(void);
+
+/*
+Funcao que insere elemento inteiro na lista
+argumentos:
+    Lista * lista;
+    int valor: valor inteiro a ser inserido
+    
+Retorno:
+    Nova lista
+*/
+
+// Funcao que insere elemento inteiro na lista
+Lista * insere_elementos(Lista * lista, int valor);
+
+// Funcao que imprime os elementos da lista
+void lst_imprime(Lista * l);
+
+// Funcao que verifica se a lista esta vazia
+int lst_vazia(Lista * l);
+
+// Funcao que busca um elemento na lista
+Lista * lst_busca(Lista * l, int v);
+
+
