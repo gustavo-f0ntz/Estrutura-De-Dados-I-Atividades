1c0bb1d4220d45dbd87d21ba5b214649ed339422
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2024-03-04 16:10:09 -0300

    Iniciando estudos de Listas Encadeadas

---
+++ 3 - Unidade/Listas Encadeadas/lista_enc.c
@@ -0,0 +1,11 @@
+// Lista Encadeada
+
+// -- Espaco de memoria proporcional ao numero de elementos da lista.
+
+// -- Nao ha garantia de armazenamento contiguo.
+// -- Cada no estara conectado a outro no por um ponteiro que aponta para o proximo no e assim por diante.
+// -- É importante que o ultimo elemento da lista aponte para NULL.
+
+
+
+
